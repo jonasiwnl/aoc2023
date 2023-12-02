8bc00fc12f67e95e1cc12c5f062ba37a895caf4c
jonasiwnl <jonasg.13452@gmail.com>
2023-12-01 23:08:44 -0500

    revise part 2 solution with trie

--- day1/sol2.py
+++ day1/sol2.py
@@ -1,29 +1,95 @@
-SPELLED_DIGITS = [
-    'one', 'two', 'three', 'four',
-    'five', 'six', 'seven', 'eight', 'nine',
-]
-VALUES = {
-    'one': 1, 'two': 2, 'three': 3, 'four': 4,
-    'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
-    'nine': 9,
+SPELLED_DIGITS = {
+    'o': {
+        'n': {
+            'e': {
+                '#': '1', 
+            },
+        },
+    },
+    't': {
+        'w': {
+            'o': {
+                '#': '2',
+            },
+        },
+        'h': {
+            'r': {
+                'e': {
+                    'e': {
+                        '#': '3',
+                    },
+                },
+            },
+        },
+    },
+    'f': {
+        'o': {
+            'u': {
+                'r': {
+                    '#': '4',
+                },
+            },
+        },
+        'i': {
+            'v': {
+                'e': {
+                    '#': '5',
+                },
+            },
+        },
+    },
+    's': {
+        'i': {
+            'x': {
+                '#': '6',
+            },
+        },
+        'e': {
+            'v': {
+                'e': {
+                    'n': {
+                        '#': '7',
+                    },
+                },
+            },
+        },
+    },
+    'e': {
+        'i': {
+            'g': {
+                'h': {
+                    't': {
+                        '#': '8',
+                    },
+                },
+            },
+        },
+    },
+    'n': {
+        'i': {
+            'n': {
+                'e': {
+                    '#': '9',
+                },
+            },
+        },
+    },
 }
 
-def is_digit_or_spelled(word):
-    if word[0].isdigit():
-        return True
-    for sd in SPELLED_DIGITS:
-        if word.startswith(sd):
-            return True
-    return False
-
-def to_value(word):
-    if word[0].isdigit():
-        return word[0]
-    for sd in SPELLED_DIGITS:
-        if word.startswith(sd):
-            return str(VALUES[sd])
+def find_value(word: str, left: bool) -> str:
+    i = 0 if left else len(word) - 1
+    while True:
+        if word[i].isdigit():
+            return word[i]
         
-    raise ValueError('Not a digit or spelled digit')
+        trie = SPELLED_DIGITS
+        j = i
+        while j < len(word) and word[j] in trie:
+            trie = trie[word[j]]
+            if '#' in trie: return trie['#']
+            j += 1
+
+        i += 1 if left else -1
 
 def main():
     sum_calibration = 0
@@ -31,17 +97,10 @@ def main():
     with open('input', 'r') as f:
         lines = f.readlines()
         for line in lines:
-            left = 0
-            right = len(line) - 1
-
-            while not is_digit_or_spelled(line[left:]):
-                left += 1
-            while not is_digit_or_spelled(line[right:]):
-                right -= 1
-
-            sum_calibration += int(to_value(line[left:]) + to_value(line[right:]))
+            sum_calibration += int(find_value(line, True) + find_value(line, False))
 
     print(sum_calibration)
 
 if __name__ == '__main__':
     main()
+
